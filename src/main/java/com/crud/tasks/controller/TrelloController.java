264581f81bc241bfe9218bf9fd4c98b0007f88a4
LENOVO-PC\Paweł <pawel.dz.m@interia.pl>
2019-03-07 12:24:07 +0100

    Tests- test 2 failed, others good

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -19,20 +19,25 @@ public class TrelloController {
     @Autowired
     private TrelloClient trelloClient;
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoardsPublic();
-
-        trelloBoards.stream()
-                .filter(t -> t.getId() != null && t.getName() != null && t.getName().contains("Kodilla"))
-
-                .forEach(trelloBoardDto -> {
-                    System.out.println(trelloBoardDto.getName()+"-"+trelloBoardDto.getId());
-                    trelloBoardDto.getLists().forEach(trelloList -> System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
-
-
-                });
+//    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
+//    public void getTrelloBoards() {
+//
+//        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoardsPublic();
+//
+//        trelloBoards.stream()
+//                .filter(t -> t.getId() != null && t.getName() != null && t.getName().contains("Kodilla"))
+//
+//                .forEach(trelloBoardDto -> {
+//                    System.out.println(trelloBoardDto.getName()+"-"+trelloBoardDto.getId());
+//                    trelloBoardDto.getLists().forEach(trelloList -> System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+//
+//
+//                });
+//    }
+    @RequestMapping(method=RequestMethod.GET, value="getTrelloBoards")
+    public List<TrelloBoardDto> getTrelloBoards()
+    {
+        return trelloClient.getTrelloBoardsPublic();
     }
     @RequestMapping(method=RequestMethod.POST, value="createTrelloCard")
     public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto)
